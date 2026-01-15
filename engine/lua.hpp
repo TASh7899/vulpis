16300eda5d7aa73a7742dc5d76a343ea58f71b4f
plumsy-plum <shubhamjha2864@gmail.com>
2026-01-15 19:09:29 +0530

    input handling : input router, functional styling, focus/state

--- engine/lua.hpp
+++ engine/lua.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
+// Ensure we do not build against a Lua DLL import layout when linking statically
+#ifdef LUA_BUILD_AS_DLL
+#undef LUA_BUILD_AS_DLL
+#endif
+
+// Add this line to tell the compiler we are using Lua as a static library
+#ifndef LUA_CORE
+#define LUA_LIB
+#endif
+
 extern "C" {
     #include <lua.h>
     #include <lauxlib.h>
