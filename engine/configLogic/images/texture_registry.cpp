a58dd2abf79e89c3b6fbcf1958f82e01a9d04d1d
TASh7899 <tash64934@gmail.com>
2026-03-14 14:32:52 +0530

    feat: adds better image compression for baked textures
    - adds DXT5 compression for .vtex files
    - fixes pixel alignment glitch for odd-sized images

--- engine/configLogic/images/texture_registry.cpp
+++ engine/configLogic/images/texture_registry.cpp
@@ -21,6 +21,12 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "../../../third_party/stb_image/stb_image.h"
 
+#define STB_DXT_IMPLEMENTATION
+#include "../../../third_party/stb_image/stb_dxt.h"
+
+#ifndef GL_COMPRESSED_RGBA_S3TC_DXT5_EXT
+#define GL_COMPRESSED_RGBA_S3TC_DXT5_EXT 0x83F3
+#endif
 
 namespace TextureRegistry {
 
@@ -29,6 +35,7 @@ namespace TextureRegistry {
     GLuint pbo;
     int width;
     int height;
+    size_t dataSize;
   };
 
   struct TextureInfo {
@@ -41,93 +48,108 @@ namespace TextureRegistry {
   static std::vector<UploadTask> uploadQueue;
   static std::mutex queueMutex;
 
-
-  GLuint GetTexture(const std::string &path) {
-    if (path.empty()) return 0;
-
-    auto it = textureCache.find(path);
-    if (it != textureCache.end()) {
-      it->second.refCount++;
-      return it->second.id;
-    }
-
-    namespace fs = std::filesystem;
-    std::string relativePath = path;
-    if (relativePath.find("assets/") == 0) {
-      relativePath = relativePath.substr(7);
-    }
-
-    fs::path cachePath = std::filesystem::path(Vulpis::getProjectRoot()) / "baked" / relativePath;
-    cachePath.replace_extension(".vtex");
-
-    std::string origPath = Vulpis::getProjectRoot() + "/" + path;
-    std::string cachePathStr = cachePath.string();
-
-    int w = 0, h = 0;
-    bool needsBake = false;
-
-    std::ifstream file(cachePath, std::ios::binary);
-    if (file) {
-      file.read(reinterpret_cast<char*>(&w), sizeof(int));
-      file.read(reinterpret_cast<char*>(&h), sizeof(int));
-      file.close();
-    } else {
-      int comp;
-      if (!stbi_info(origPath.c_str(), &w, &h, &comp)) {
-        std::cerr << "TEXTURE ERROR: Asset missing entirely: " << origPath << std::endl;
-        return 0;
+void CompressToDXT5(const unsigned char* rgba, int w, int h, unsigned char* out_dxt) {
+  int blocksW = (w + 3) / 4;
+  int blocksH = (h + 3) / 4;
+  for (int y = 0; y < blocksH; ++y) {
+    for (int x = 0; x < blocksW; ++x) {
+      unsigned char block[64];
+      for (int by = 0; by < 4; ++by) {
+        int py = std::min(y * 4 + by, h - 1);
+        for (int bx = 0; bx < 4; ++bx) {
+          int px = std::min(x * 4 + bx, w - 1);
+          int srcIdx = (py * w + px) * 4;
+          int dstIdx = (by * 4 + bx) * 4;
+          std::memcpy(&block[dstIdx], &rgba[srcIdx], 4);
+        }
       }
-      needsBake = true;
+      stb_compress_dxt_block(&out_dxt[(y * blocksW + x) * 16], block, 1, STB_DXT_NORMAL);
     }
+  }
+}
 
-    size_t dataSize = w * h * 4;
+GLuint GetTexture(const std::string &path) {
+  if (path.empty()) return 0;
+  auto it = textureCache.find(path);
+  if (it != textureCache.end()) {
+    it->second.refCount++;
+    return it->second.id;
+  }
 
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
+  namespace fs = std::filesystem;
+  fs::path rootPath = Vulpis::getProjectRoot();
+  fs::path originalFileFullPath = rootPath / path;
+  std::string relativePath = path;
+  if (relativePath.find("assets/") == 0) relativePath = relativePath.substr(7);
+  else if (relativePath.find("./assets/") == 0) relativePath = relativePath.substr(9);
 
-    unsigned char emptyPixel[] = {0, 0, 0, 0};
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, emptyPixel);
+  fs::path cachePath = rootPath / "baked" / relativePath;
+  cachePath.replace_extension(".vtex");
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  std::string origPath = originalFileFullPath.string();
+  std::string cachePathStr = cachePath.string();
 
-    textureCache[path] = {textureID, 1};
-    idToPath[textureID] = path;
+  int w = 0, h = 0;
+  bool needsBake = false;
 
-    GLuint pbo;
-    glGenBuffers(1, &pbo);
-    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+  std::ifstream file(cachePath, std::ios::binary);
+  if (file) {
+    file.read(reinterpret_cast<char*>(&w), sizeof(int));
+    file.read(reinterpret_cast<char*>(&h), sizeof(int));
+    file.close();
+  } else {
+    int comp;
+    if (!stbi_info(origPath.c_str(), &w, &h, &comp)) {
+      std::cerr << "TEXTURE ERROR: Asset missing entirely: " << origPath << std::endl;
+      return 0;
+    }
+    needsBake = true;
+  }
 
-    glBufferData(GL_PIXEL_UNPACK_BUFFER, dataSize, nullptr, GL_STREAM_DRAW);
-    void* mappedPtr = glMapBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, dataSize, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
-    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+  size_t dataSize = ((w + 3) / 4) * ((h + 3) / 4) * 16;
 
-    std::thread([textureID, pbo, mappedPtr, dataSize, w, h, cachePathStr, origPath, needsBake]() { // <-- Added missing variables here!
+  GLuint textureID;
+  glGenTextures(1, &textureID);
+  glBindTexture(GL_TEXTURE_2D, textureID);
+  unsigned char emptyPixel[] = {0, 0, 0, 0};
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, emptyPixel);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  textureCache[path] = {textureID, 1};
+  idToPath[textureID] = path;
+
+  GLuint pbo;
+  glGenBuffers(1, &pbo);
+  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+  glBufferData(GL_PIXEL_UNPACK_BUFFER, dataSize, nullptr, GL_STREAM_DRAW);
+  void* mappedPtr = glMapBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, dataSize, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+
+  std::thread([textureID, pbo, mappedPtr, dataSize, w, h, cachePathStr, origPath, needsBake]() {
       if (needsBake) {
         int tw, th, tc;
         unsigned char* pixels = stbi_load(origPath.c_str(), &tw, &th, &tc, STBI_rgb_alpha);
-
         if (pixels && mappedPtr) {
-          std::memcpy(mappedPtr, pixels, dataSize);
+          CompressToDXT5(pixels, tw, th, static_cast<unsigned char*>(mappedPtr));
+          stbi_image_free(pixels); 
 
           std::filesystem::create_directories(std::filesystem::path(cachePathStr).parent_path());
           std::ofstream out(cachePathStr, std::ios::binary);
           if (out) {
             out.write(reinterpret_cast<char*>(&tw), sizeof(int));
             out.write(reinterpret_cast<char*>(&th), sizeof(int));
-            out.write(reinterpret_cast<char*>(pixels), dataSize);
+            out.write(reinterpret_cast<const char*>(mappedPtr), dataSize);
           }
-          stbi_image_free(pixels);
 
           std::lock_guard<std::mutex> lock(queueMutex);
-          uploadQueue.push_back({textureID, pbo, w, h});
+          uploadQueue.push_back({textureID, pbo, tw, th, dataSize}); 
         } else {
           if (pixels) stbi_image_free(pixels);
           std::lock_guard<std::mutex> lock(queueMutex);
-          uploadQueue.push_back({0, pbo, 0, 0});
+          uploadQueue.push_back({0, pbo, 0, 0, 0});
         }
       } else {
         std::ifstream t_file(cachePathStr, std::ios::binary);
@@ -135,64 +157,66 @@ namespace TextureRegistry {
           t_file.seekg(8);
           t_file.read(reinterpret_cast<char*>(mappedPtr), dataSize);
           std::lock_guard<std::mutex> lock(queueMutex);
-          uploadQueue.push_back({textureID, pbo, w, h});
+          uploadQueue.push_back({textureID, pbo, w, h, dataSize});
         } else {
           std::lock_guard<std::mutex> lock(queueMutex);
-          uploadQueue.push_back({0, pbo, 0, 0});
+          uploadQueue.push_back({0, pbo, 0, 0, 0});
         }
       }
-    }).detach();
+  }).detach();
 
-      return textureID;
+  return textureID;
+}
+
+void ReleaseTexture(GLuint textureID) {
+  if (textureID == 0) return;
+  auto pathIt = idToPath.find(textureID);
+  if (pathIt == idToPath.end()) return;
+  std::string path = pathIt->second;
+  auto cacheIt = textureCache.find(path);
+  if (cacheIt != textureCache.end()) {
+    cacheIt->second.refCount--;
+    if (cacheIt->second.refCount <= 0) {
+      glDeleteTextures(1, &cacheIt->second.id);
+      textureCache.erase(cacheIt);
+      idToPath.erase(pathIt);
     }
-
-    void ReleaseTexture(GLuint textureID) {
-      if (textureID == 0) return;
-      auto pathIt = idToPath.find(textureID);
-      if (pathIt == idToPath.end()) {
-        return;
-      }
-      std::string path = pathIt->second;
-      auto cacheIt = textureCache.find(path);
-      if (cacheIt != textureCache.end()) {
-        cacheIt->second.refCount--;
-
-        if (cacheIt->second.refCount <= 0) {
-          glDeleteTextures(1, &cacheIt->second.id);
-          textureCache.erase(cacheIt);
-          idToPath.erase(pathIt);
-        }
-      }
-    }
-
-    bool ProcessUploads() {
-      std::lock_guard<std::mutex> lock(queueMutex);
-
-      if (uploadQueue.empty()) return false;
-
-      for (const auto& task : uploadQueue ) {
-        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, task.pbo);
-        glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
-
-        if (task.targetID != 0 && idToPath.find(task.targetID) != idToPath.end()) {
-          glBindTexture(GL_TEXTURE_2D, task.targetID);
-          glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, task.width, task.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-        }
-
-        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
-        glDeleteBuffers(1, &task.pbo);
-
-      }
-      uploadQueue.clear();
-      return true;
-    }
-
-    void Cleanup() {
-      for (const auto& pair : textureCache) {
-        glDeleteTextures(1, &pair.second.id);
-      }
-      textureCache.clear();
-      idToPath.clear();
-    }
-
   }
+}
+
+bool ProcessUploads() {
+  std::lock_guard<std::mutex> lock(queueMutex);
+  if (uploadQueue.empty()) return false;
+
+  for (const auto& task : uploadQueue ) {
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, task.pbo);
+    glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+
+    if (task.targetID != 0 && idToPath.find(task.targetID) != idToPath.end()) {
+      glBindTexture(GL_TEXTURE_2D, task.targetID);
+      glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+      glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+      glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0);
+      glPixelStorei(GL_UNPACK_SKIP_ROWS, 0);
+
+      int paddedW = ((task.width + 3) / 4) * 4;
+      int paddedH = ((task.height + 3) / 4) * 4;
+
+      glCompressedTexImage2D(GL_TEXTURE_2D, 0, GL_COMPRESSED_RGBA_S3TC_DXT5_EXT, paddedW, paddedH, 0, task.dataSize, nullptr);
+    }
+
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+    glDeleteBuffers(1, &task.pbo);
+  }
+  uploadQueue.clear();
+  return true;
+}
+
+void Cleanup() {
+  for (const auto& pair : textureCache) {
+    glDeleteTextures(1, &pair.second.id);
+  }
+  textureCache.clear();
+  idToPath.clear();
+}
+}
--- engine/tools/asset_baker.cpp
+++ engine/tools/asset_baker.cpp
@@ -1,15 +1,39 @@
-#include <complex>
 #include <iostream>
 #include <filesystem>
 #include <fstream>
 #include <string>
 #include <algorithm>
 #include <system_error>
+#include <cstring>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "../../third_party/stb_image/stb_image.h"
 
+#define STB_DXT_IMPLEMENTATION
+#include "../../third_party/stb_image/stb_dxt.h"
+
 namespace fs = std::filesystem;
+
+void CompressToDXT5(const unsigned char* rgba, int w, int h, unsigned char* out_dxt) {
+  int blocksW = (w + 3) / 4;
+  int blocksH = (h + 3) / 4;
+
+  for (int y = 0; y < blocksH; ++y) {
+    for (int x = 0; x < blocksW; ++x) {
+      unsigned char block[64];
+      for (int by = 0; by < 4; ++by) {
+        int py = std::min(y * 4 + by, h - 1);
+        for (int bx = 0; bx < 4; ++bx) {
+          int px = std::min(x * 4 + bx, w - 1);
+          int srcIdx = (py * w + px) * 4;
+          int dstIdx = (by * 4 + bx) * 4; 
+          std::memcpy(&block[dstIdx], &rgba[srcIdx], 4);
+        }
+      }
+      stb_compress_dxt_block(&out_dxt[(y * blocksW + x) * 16], block, 1 /* has_alpha */, STB_DXT_NORMAL);
+    }
+  }
+}
 
 void bakeTextures(const fs::path& imgPath, const fs::path& outPath) {
   fs::path cachePath = outPath;
@@ -33,12 +57,18 @@ void bakeTextures(const fs::path& imgPath, const fs::path& outPath) {
     return;
   }
 
+  size_t dataSize = ((w + 3) / 4) * ((h + 3) / 4) * 16;
+  unsigned char* outData = new unsigned char[dataSize];
+  CompressToDXT5(pixels, w, h, outData);
+
   std::ofstream file(cachePath, std::ios::binary);
   if (file) {
     file.write(reinterpret_cast<const char*>(&w), sizeof(int));
     file.write(reinterpret_cast<const char*>(&h), sizeof(int));
-    file.write(reinterpret_cast<const char*>(pixels), w * h * 4);
+    file.write(reinterpret_cast<const char*>(outData), dataSize);
   }
+  
+  delete[] outData;
   stbi_image_free(pixels);
 }
 
@@ -68,9 +98,6 @@ int main(int argc, char* argv[]) {
       }
     }
   }
-
   std::cout << "Asset baking complete.\n";
   return 0;
-
 }
-
